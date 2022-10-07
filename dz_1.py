8e11515aa0fa1dafbdd43a70ca9b01e7c22b87e9
andrn92 <andrn92@yandex.ru>
2022-10-07 23:16:14 +0300

    Add files

---
+++ dz_1.py
@@ -0,0 +1,24 @@
+import requests
+
+
+url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
+response = requests.get(url)
+data_heroes = response.json()
+list_heroes = ['Hulk', 'Captain America', 'Thanos']
+new_list_heroes = []
+
+for data_hero in data_heroes:
+    dict_heroes = {}
+    if data_hero['name'] in list_heroes:
+        dict_heroes['name'] = data_hero['name']
+        dict_heroes['intelligence'] = data_hero['powerstats']['intelligence']
+        new_list_heroes.append(dict_heroes)
+        
+print(new_list_heroes)
+maximum = 0
+for data_hero in new_list_heroes:
+    if data_hero['intelligence'] > maximum:
+        maximum = data_hero['intelligence']
+        hero_name = data_hero['name']
+
+print(hero_name)
---
+++ dz_2.py
@@ -0,0 +1,46 @@
+from pprint import pprint
+
+import requests
+
+
+class YaUploader:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {self.token}'
+        }
+
+    def get_files_list(self):
+        files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
+        headers = self.get_headers()
+        response = requests.get(files_url, headers=headers)
+        return response.json()
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload(self, disk_file_path, filename):
+        link_dict = self._get_upload_link(disk_file_path=disk_file_path)
+        href = link_dict["href"]
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    path_to_file = ...
+    path_to_disk = ...
+    token = ...
+    uploader = YaUploader(token=token)
+    pprint(uploader.upload(path_to_disk, path_to_file))
+
