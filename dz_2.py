c9fee866db2d49e0e3ef44b9f698b70d32b1cd1d
andrn92 <andrn92@yandex.ru>
2022-10-11 09:48:23 +0300

    Change variable

--- dz_2.py
+++ dz_2.py
@@ -19,27 +19,27 @@ class YaUploader:
         response = requests.get(files_url, headers=headers)
         return response.json()
 
-    def _get_upload_link(self, disk_file_path):
+    def _get_upload_link(self, path_to_disk):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-        params = {"path": disk_file_path, "overwrite": "true"}
+        params = {"path": path_to_disk, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
         pprint(response.json())
         return response.json()
 
-    def upload(self, disk_file_path, filename):
-        link_dict = self._get_upload_link(disk_file_path=disk_file_path)
+    def upload(self, path_to_disk, file_name):
+        link_dict = self._get_upload_link(path_to_disk=path_to_disk)
         href = link_dict["href"]
-        response = requests.put(href, data=open(filename, 'rb'))
+        response = requests.put(href, data=open(file_name, 'rb'))
         response.raise_for_status()
         if response.status_code == 201:
             print("Success")
 
 
 if __name__ == '__main__':
-    path_to_file = ...
+    file_name = ...
     path_to_disk = ...
     token = ...
     uploader = YaUploader(token=token)
-    pprint(uploader.upload(path_to_disk, path_to_file))
+    pprint(uploader.upload(path_to_disk, file_name))
 
